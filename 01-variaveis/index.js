9c0b8190a0061b3274b0bcbd462446cb6696cddf
AndrewHGomes <andrewvianigomes@gmail.com>
2025-01-03 17:59:35 -0300

    index.js na pasta de variáveis

---
+++ 01-variaveis/index.js
@@ -0,0 +1,16 @@
+const umaString = prompt("Digite algo textual:");
+
+console.log(`${umaString} | ${typeof umaString}`);
+alert(`${umaString} | ${typeof umaString}`);
+
+const umNumber = Number(prompt("Digite algum número:"));
+
+console.log(`${umNumber} | ${typeof umNumber}`);
+alert(`${umNumber} | ${typeof umNumber}`);
+
+const umBoolean = confirm("OK para true ou CANCEL para false:");
+
+console.log(`${umBoolean} | ${typeof umBoolean}`);
+alert(`${umBoolean} | ${typeof umBoolean}`);
+
+console.log("");
