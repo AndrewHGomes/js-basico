7cbd0e48acaee493e090ed873f3d878d3edeef53
AndrewHGomes <andrewvianigomes@gmail.com>
2025-01-02 22:30:53 -0300

    Mais sobre arrays e objetos

---
+++ 06-arrays/destructuring.js
@@ -0,0 +1,7 @@
+const listaCompras = ["pão", "leite", "manteiga", "ovos"];
+
+console.log(listaCompras);
+
+const [item1, item2, item3, item4] = listaCompras;
+
+console.log(item1, item2, item3, item4);
