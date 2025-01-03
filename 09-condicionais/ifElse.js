d9efd9a31d1d52dfcc9154ccd1886cac83c41f34
AndrewHGomes <andrewvianigomes@gmail.com>
2025-01-03 00:01:54 -0300

    Melhorias em condicionais e iterações

---
+++ 09-condicionais/ifElse.js
@@ -0,0 +1,9 @@
+if (idade > 18) {
+  console.log("Você é maior de idade.");
+  console.log("Já pode ter CNH.");
+} else {
+  console.log("Você é menor de idade.");
+  console.log("Não pode ter CNH.");
+}
+
+console.log("");
---
+++ 10-iteracoes/doWhile.js
@@ -0,0 +1,13 @@
+let dw = 0;
+
+do {
+  console.log(`Do While: ${dw}`);
+  dw++;
+} while (dw < 2);
+
+do {
+  console.log(`Do While: ${dw}`);
+  dw--;
+} while (dw >= 0);
+
+console.log("");
