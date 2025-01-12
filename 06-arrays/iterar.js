0e2eb7eb7449fed7d1c12d24dd95da2147417bad
AndrewHGomes <andrewvianigomes@gmail.com>
2025-01-12 15:20:40 -0300

    Anotções e exercícios

--- 06-arrays/iterar.js
+++ 06-arrays/iterar.js
@@ -1,30 +1,25 @@
-let contador = 0;
+const pilotos = ["Senna", "Prost", "Alonso", "Schumacher", "Hamilton"];
 
-while (contador < nomes.length) {
-  console.log(`While: ${contador + 1}º -> ${nomes[contador]}`);
-  contador++;
+for (let piloto in pilotos) {
+  console.log(`For in: ${piloto} -> ${pilotos[piloto]}`);
 }
 
 console.log("");
 
-for (let i = 0; i < nomes.length; i++) {
-  console.log(`For: ${i + 1}º -> ${nomes[i]}`);
+for (let piloto of pilotos) {
+  console.log(`For of: ${piloto}`);
 }
 
 console.log("");
 
-for (let nome in nomes) {
-  console.log(`For in: ${nome} -> ${nomes[nome]}`);
-}
-
-console.log("");
-
-for (let nome of nomes) {
-  console.log(`For of: ${nome}`);
-}
-
-console.log("");
-
-nomes.forEach((nome, index) => {
-  console.log(`For Each: ${nome} na posição ${index}`);
+pilotos.forEach((piloto, index) => {
+  console.log(`For Each: ${piloto} na posição ${index}`);
 });
+
+console.log("");
+
+const alonso = pilotos.find((piloto) => {
+  piloto === "Alonso";
+});
+
+console.log(alonso);
