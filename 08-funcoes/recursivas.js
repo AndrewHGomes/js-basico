6af339bac2a1caaab3f4433902f55788ce7c3723
AndrewHGomes <andrewvianigomes@gmail.com>
2025-01-13 22:14:20 -0300

    Funções recursivas

--- 08-funcoes/recursivas.js
+++ 08-funcoes/recursivas.js
@@ -10,3 +10,19 @@ function dividir(num) {
 }
 
 dividir(100);
+
+function fatorial(num) {
+  console.log(`Fatorial de: ${num}`);
+  if (num === 0) {
+    console.log("Fatorial de 0 é 1");
+    return 1;
+  } else if (num === 1) {
+    console.log("Fatorial de 1 é 1");
+    return 1;
+  } else {
+    console.log(`${num} x !${num - 1}`);
+    return num * fatorial(num - 1);
+  }
+}
+
+console.log(fatorial(10));
